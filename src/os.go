b9abebac86798327ae6a1bd629681a1dab4f1aff
lxsh-S <kumarlakshya973@gmail.com>
2026-05-28 20:46:28 +0530

    feat: os.go -> return OS and username

---
+++ src/os.go
@@ -0,0 +1,19 @@
+package src
+
+import (
+	"os"
+)
+
+// in util.go we have the GetSystemKey func that will extract the value of "PRETTY_NAME" and will return it
+func GetOSName() string {
+	return GetSystemKey("etc/os-release", "PRETTY_NAME")
+}
+
+// returning username
+func GetUsername() string {
+	username := os.Getenv("USER") // we know about $USER right? :)
+	if username == "" {           // I dont think $USER will be blank but just so it doesnt crash
+		return "user"
+	}
+	return username
+}
