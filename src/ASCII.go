50bfa6c935b5f8235569c73c1ee3e5231ceec901
lxsh-S <kumarlakshya973@gmail.com>
2026-05-28 21:37:33 +0530

    custom ascii art prototype

---
+++ src/ASCII.go
@@ -0,0 +1,25 @@
+package src
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func PrintASCII() string {
+	filePath := "src/data/ascii.txt"
+	file, err := os.Open(filePath) // search for ascii
+	if err != nil {
+		return fmt.Sprintf("error opening ASCII.txt: %v", err)
+	}
+	defer file.Close()
+
+	ascii, err1 := io.ReadAll(file)
+	if err != nil {
+		return fmt.Sprintf("error reading ASCII.txt: %v", err1)
+	}
+
+	asciiString := string(ascii)
+
+	return asciiString
+}
