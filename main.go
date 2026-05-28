e09662b87eafb673af73be4b2ff594c0fdebd655
lxsh-S <kumarlakshya973@gmail.com>
2026-05-28 21:00:23 +0530

    feat: Print ASCII art with system information

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
+// Gofie
 package main
+
+import (
+	"fmt"
+
+	"github.com/lxsh-S/gofie/src"
+)
+
+func main() {
+	//les get our data
+	//
+	OS := src.GetOSName()
+	username := src.GetUsername()
+
+	// lets define terminal ANSI style
+	cyan := "\033[36m" // cyan
+	bold := "\033[1m"  // bold text
+	reset := "\033[0m" // clear all things
+
+	fmt.Println(" ")
+
+	fmt.Printf("%s    /\\_/\\    %s%s%s@%sDesktop:%s\n", cyan, reset, bold, username, cyan, reset) // look like[Desktop: lxsh-S]
+	fmt.Printf("%s   ( o.o )   %s---------------------\n", cyan, reset)
+	fmt.Printf("%s    > ^ <    %s%sOS:%s       %s\n", cyan, reset, cyan, reset, OS) // look like[os: Arch linux]
+	fmt.Printf("%s   /     \\   %s\n", cyan, reset)
+	fmt.Printf("%s  (_/(_/\\_)  %s\n", cyan, reset)
+	fmt.Println(" ")
+	fmt.Println(" ")
+}
--- src/utils.go
+++ src/utils.go
@@ -7,10 +7,10 @@ import (
 )
 
 // This func will open a file and look for a specific label inside
-func GetSystemKey(filePath, key string) (string, error) {
+func GetSystemKey(filePath, key string) string {
 	file, err := os.Open(filePath)
 	if err != nil {
-		return " ", err
+		return "Unkown error occured when opening the file"
 	}
 	defer file.Close()
 
@@ -24,7 +24,7 @@ func GetSystemKey(filePath, key string) (string, error) {
 			parts := strings.Split(line, ":") // we'll handel the data that is seperated by ":"
 			if len(parts) > 1 {
 				data := strings.TrimSpace(parts[1])
-				return data, nil
+				return data
 			}
 		}
 
@@ -33,9 +33,9 @@ func GetSystemKey(filePath, key string) (string, error) {
 			parts := strings.Split(line, "=")
 			if len(parts) > 1 {
 				data := strings.TrimSpace(parts[1])
-				return data, nil
+				return data
 			}
 		}
 	}
-	return "done", nil
+	return "key not found" // we'll only reach this line when the scanner didnt find a key in the file
 }
