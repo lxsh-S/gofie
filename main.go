ff9d260b189eaa4c63827c6d84c844e580f268a2
lxsh-S <kumarlakshya973@gmail.com>
2026-06-01 16:34:04 +0530

    feat: yaygit add . finally got a prototype

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	cyan := "\033[36m" // cyan
 	bold := "\033[1m"  // bold text
 	reset := "\033[0m" // clear all things
+	red := "\033[31m"
 
 	asciiBlock := src.PrintASCII()
 	asciiLines := strings.Split(asciiBlock, "\n")
@@ -24,30 +25,94 @@ func main() {
 
 		OS := src.GetOSNameDarwin()
 		username := src.GetUserNameDarwin()
-		ascii := src.PrintASCII()
-		fmt.Println(ascii)
+		// ascii := src.PrintASCII()
+		// fmt.Println(ascii)
 
+		infolines := []string{
+			fmt.Sprintf("%s%s@%sDesktop:%s", bold, username, red, reset),
+			"------------------",
+			fmt.Sprintf("%sOS:%s %s%s%s%s", red, reset, bold, cyan, OS, reset),
+			// fmt.Sprintf("%sCPU:%s %s%s%s%s"), //empty for now
+			// fmt.Sprintf("%sMemory:%s %s%s%s%s"), //Same
+		}
 		fmt.Println(" ")
 
-		fmt.Printf("%s%s%s%s@%sDesktop:%s\n", cyan, reset, bold, username, cyan, reset)
-		fmt.Printf("%s%s---------------------\n", cyan, reset)
-		fmt.Printf("%s%s%sOS:%s       %s\n", cyan, reset, cyan, reset, OS)
-		fmt.Println(" ")
+		// Lets check max height
+		maxlines := len(asciiLines)
+		if len(infolines) > maxlines {
+			maxlines = len(infolines)
+		}
+
+		// Print side on side
+		for i := 0; i < maxlines; i++ {
+			asciiPar := ""
+			infoPar := ""
+
+			if i < len(asciiLines) {
+				asciiPar = asciiLines[i]
+			}
+
+			if i < len(infolines) {
+				infoPar = infolines[i]
+			}
+
+			fmt.Printf("%-55s   %s\n", asciiPar, infoPar)
+
+		}
 		fmt.Println(" ")
 
+		// fmt.Printf("%s%s%s%s@%sDesktop:%s\n", cyan, reset, bold, username, cyan, reset)
+		// fmt.Printf("%s%s---------------------\n", cyan, reset)
+		// fmt.Printf("%sOS:%s%s       %s\n", cyan, reset,red, OS)
+		// fmt.Println(" ")
+		// fmt.Println(" ")
+		//
 	case "linux":
 
 		OS := src.GetOsName()
 		username := src.GetUserName()
-		ascii := src.PrintASCII()
-		fmt.Println(ascii)
+		// ascii := src.PrintASCII()
+		// fmt.Println(ascii)
 
+		infolines := []string{
+			fmt.Sprintf("%s%s@%sDesktop:%s", bold, username, red, reset),
+			"------------------",
+			fmt.Sprintf("%sOS:%s %s%s%s%s", red, reset, bold, cyan, OS, reset),
+			// fmt.Sprintf("%sCPU:%s %s%s%s%s"), //empty for now
+			// fmt.Sprintf("%sMemory:%s %s%s%s%s"), //Same
+		}
 		fmt.Println(" ")
 
-		fmt.Printf("%s%s%s%s@%sDesktop:%s\n", cyan, reset, bold, username, cyan, reset)
-		fmt.Printf("%s%s---------------------\n", cyan, reset)
-		fmt.Printf("%s%s%sOS:%s       %s\n", cyan, reset, cyan, reset, OS)
+		// Lets check max height
+		maxlines := len(asciiLines)
+		if len(infolines) > maxlines {
+			maxlines = len(infolines)
+		}
+
+		// Print side on side
+		for i := 0; i < maxlines; i++ {
+			asciiPar := ""
+			infoPar := ""
+
+			if i < len(asciiLines) {
+				asciiPar = asciiLines[i]
+			}
+
+			if i < len(infolines) {
+				infoPar = infolines[i]
+			}
+
+			fmt.Printf("%-55s   %s\n", asciiPar, infoPar) // we'll print it left side and fill spaces too
+
+		}
 		fmt.Println(" ")
-		fmt.Println(" ")
+
+		// fmt.Println(" ")
+
+		// fmt.Printf("%s%s%s%s@%sDesktop:%s\n", cyan, reset, bold, username, cyan, reset)
+		// fmt.Printf("%s%s---------------------\n", cyan, reset)
+		// fmt.Printf("%s%s%sOS:%s       %s\n", cyan, reset, cyan, reset, OS)
+		// fmt.Println(" ")
+		// fmt.Println(" ")
 	}
 }
